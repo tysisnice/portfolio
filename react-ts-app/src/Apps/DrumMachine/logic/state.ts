6cc46ff8582f97814954a6fd4bd5041ad1071b89
tysisnice <tysisnice@gmail.com>
2019-08-19 16:07:58 +0700

    trying to get deploy working 2

--- react-ts-app/src/Apps/DrumMachine/logic/state.ts
+++ react-ts-app/src/Apps/DrumMachine/logic/state.ts
@@ -19,15 +19,15 @@ export default class DrumMachineState {
 }
 
 const initialSoundBoard: IDrumPad[] = [
-  { char: 'q', audio: '/audio/s-burp.mp3', name: 'burp' },
-  { char: 'w', audio: '/audio/s-beer-burp.mp3', name: 'big burp' },
-  { char: 'e', audio: '/audio/s-little-burp.mp3', name: 'little burp' },
-  { char: 'a', audio: '/audio/s-dinosaur-burp.mp3', name: 'dinosaur burp' },
-  { char: 's', audio: '/audio/s-long-burp.mp3', name: 'long burp' },
-  { char: 'd', audio: '/audio/s-deep-burp.mp3', name: 'deep burp' },
-  { char: 'z', audio: '/audio/s-ah-yeah.mp3', name: 'ah yeah'},
-  { char: 'x', audio: '/audio/s-fart.mp3', name: 'fart' },
-  { char: 'c', audio: '/audio/s-cough.mp3', name: 'cough' },
+  { char: 'q', audio: 'audio/s-burp.mp3', name: 'burp' },
+  { char: 'w', audio: 'audio/s-beer-burp.mp3', name: 'big burp' },
+  { char: 'e', audio: 'audio/s-little-burp.mp3', name: 'little burp' },
+  { char: 'a', audio: 'audio/s-dinosaur-burp.mp3', name: 'dinosaur burp' },
+  { char: 's', audio: 'audio/s-long-burp.mp3', name: 'long burp' },
+  { char: 'd', audio: 'audio/s-deep-burp.mp3', name: 'deep burp' },
+  { char: 'z', audio: 'audio/s-ah-yeah.mp3', name: 'ah yeah'},
+  { char: 'x', audio: 'audio/s-fart.mp3', name: 'fart' },
+  { char: 'c', audio: 'audio/s-cough.mp3', name: 'cough' },
 ]
 
 export const initialState = new DrumMachineState(initialSoundBoard);
