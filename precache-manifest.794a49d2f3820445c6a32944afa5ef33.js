88ec0836961caf7aba5c943062e7bf403715db69
tysisnice <tysisnice@gmail.com>
2019-08-19 16:01:15 +0700

    Updates

---
+++ precache-manifest.794a49d2f3820445c6a32944afa5ef33.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87d8fdc433af8cc1fc7153119cf84109",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "17130bc58a9a40cb17ae",
+    "url": "/portfolio/static/css/main.8c7dd12f.chunk.css"
+  },
+  {
+    "revision": "67a271702190499628f5",
+    "url": "/portfolio/static/js/2.fccad012.chunk.js"
+  },
+  {
+    "revision": "641b3e188f9597357358",
+    "url": "/portfolio/static/js/3.a37705cf.chunk.js"
+  },
+  {
+    "revision": "17130bc58a9a40cb17ae",
+    "url": "/portfolio/static/js/main.ba297e3e.chunk.js"
+  },
+  {
+    "revision": "4e01d7cc72109e1e4859",
+    "url": "/portfolio/static/js/runtime~main.5f3ca948.js"
+  },
+  {
+    "revision": "3bd5b00a72b4ae55762ff84e35e38269",
+    "url": "/portfolio/static/media/copy.3bd5b00a.svg"
+  },
+  {
+    "revision": "51cd94a3b76c92f450491d0f7ccbadea",
+    "url": "/portfolio/static/media/facebook.51cd94a3.svg"
+  },
+  {
+    "revision": "9ac2c1c4aa88878245021e8da389721d",
+    "url": "/portfolio/static/media/heart.9ac2c1c4.svg"
+  },
+  {
+    "revision": "67a29a01ad2f99eafb1d10a218d4789e",
+    "url": "/portfolio/static/media/twitter.67a29a01.svg"
+  }
+]);
